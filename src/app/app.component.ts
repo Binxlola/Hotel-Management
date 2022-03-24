108912ca0bab0d5e5eb0a4ea31ba1d381ec77510
Jason Smit <38807670+Binxlola@users.noreply.github.com>
2022-03-25 10:24:45 +1300

    structure change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {StatusService} from "./shared/status.service";
+import {StatusService} from "./modules/shared/status.service";
 
 export interface StatusResponse {
   status: string
--- src/app/modules/shared/status.service.ts
+++ src/app/modules/shared/status.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {catchError, Observable, throwError} from "rxjs";
-import {environment} from "../../environments/environment";
-import {StatusResponse} from "../app.component";
+import {environment} from "../../../environments/environment";
+import {StatusResponse} from "../../app.component";
 
 @Injectable({
   providedIn: 'root'
